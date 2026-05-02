9e7e23185e7542b7fa40267c2c8a86185657ef6f
Swamykarthik <skarthik.dondapati@gmail.com>
2026-05-02 14:37:30 +0530

    updated package.json

--- team-task-manager/backend/server.js
+++ team-task-manager/backend/server.js
@@ -6,21 +6,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-// MongoDB connection
 mongoose.connect("mongodb+srv://admin:admin123@cluster0.xls9kfc.mongodb.net/taskmanager?retryWrites=true&w=majority")
 .then(() => console.log("Database Connected"))
 .catch(err => console.log(err));
-// Routes import
 const taskRoutes = require("./routes/taskRoutes");
 app.use("/tasks", taskRoutes);
-
-// Test route
 app.get("/", (req, res) => {
     res.send("Backend running");
 });
-
-// Start server
 app.listen(5000, () => {
     console.log("Server running on port 5000");
 });
