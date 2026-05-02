91b0c41daaaabdc87d97d919a02668febd8afba5
Swamykarthik <skarthik.dondapati@gmail.com>
2026-05-02 17:00:18 +0530

    updated package.json

--- team-task-manager/backend/routes/taskRoutes.js
+++ team-task-manager/backend/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/Task");
+require("../models/Task")
 
 // CREATE TASK
 router.post("/", async (req, res) => {
