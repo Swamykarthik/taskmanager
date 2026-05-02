a5ee574baef41b68f35d9df230d866d8452a934c
Swamykarthik <skarthik.dondapati@gmail.com>
2026-05-02 14:51:03 +0530

    updated server file

--- team-task-manager/backend/server.js
+++ team-task-manager/backend/server.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+require("dotenv").config();
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-mongoose.connect("mongodb+srv://admin:admin123@cluster0.xls9kfc.mongodb.net/taskmanager?retryWrites=true&w=majority")
-.then(() => console.log("Database Connected"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log("DB Error:", err));
 const taskRoutes = require("./routes/taskRoutes");
 app.use("/tasks", taskRoutes);
 app.get("/", (req, res) => {
-    res.send("Backend running");
+  res.send("Backend running successfully");
 });
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
